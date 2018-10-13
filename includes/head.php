e2cf2e871b4d42a9bcbc64c8f02d9cb761d03d31
lamorbidamacchina <43170082+lamorbidamacchina@users.noreply.github.com>
2018-10-13 17:21:23 +0200

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,15 +1,11 @@
-
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
-
-  <!-- Bootstrap Core CSS -->
-  <link  href="./assets/css/bootstrap.min.css" rel="stylesheet">
-	<style>
-	@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,400i,700');
-	</style>
-	<!-- Custom CSS -->
-  <link href="./assets/css/custom.css?<?php echo time();?>" rel="stylesheet">
-	<script src="./assets/js/jquery-3.2.1.min.js"></script>
-	<script src="./assets/js/jquery.validate.min.js"></script>
-	<script src="./assets/js/main.js?<?php echo time();?>"></script>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+<!-- Bootstrap Core CSS -->
+<link  href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+<style>
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,400i,700');
+</style>
+<!-- Custom CSS -->
+<link href="./assets/css/custom.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
